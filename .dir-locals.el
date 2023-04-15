001465e14d97c031bda2bdeb380871ff11ee2285
yalexwander <91904134+yalexwander@users.noreply.github.com>
2023-04-15 20:09:00 +0300

    Fix ctags regeneration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 (
- (projectile-tags-command . "ctags-universal -Re --kinds-php=\"-a+c+d+f+i-l-n+t-v\" -f \"%s\" %s \"%s\"")
  (nil . (
          (eval . (progn
+                   (setq-local projectile-tags-command "ctags-universal -Re --kinds-php=\"-a+c+d+f+i-l-n+t-v\" -f \"%s\" %s \"%s\"")
                    (setq-local php-project-root (projectile-project-root))
                    (setq-local phpstan-flycheck-auto-set-executable nil)
                    (setq-local phpstan-executable (concat php-project-root "vendor/phpstan/phpstan/phpstan"))
